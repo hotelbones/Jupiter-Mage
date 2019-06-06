1d7af762aecc129d4142fe0f87620968d59f0994
hotelbones <hotelbones@gmail.com>
2019-06-05 22:13:00 -0400

    Initialization

---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    public int rows = 5;
+    public int cols = 8;
+    int row;
+    int col;
+    public float tileSize = 1;
+    public GameObject [,] tile;
+    
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateGrid();
+        
+    }
+
+    public void GenerateGrid()
+    {
+        GameObject referenceTile = (GameObject)Instantiate(Resources.Load("cavefloortile"));
+       // Debug.Log(referenceTile.transform);
+        tile = new GameObject[40, 40];
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < cols; col++)
+            {
+                tile[col,row] = (GameObject)Instantiate(referenceTile, transform);
+                Debug.Log(tile[col, row]);
+
+                float posX = col * tileSize;
+                float posY = row * -tileSize;
+
+                tile[col,row].transform.position = new Vector2(posX, posY);
+            }
+
+        }
+
+      //  Debug.Log(tile[1,1].transform.position);
+        Destroy(referenceTile);
+
+        float gridW = cols * tileSize;
+        float gridH = rows * tileSize;
+        transform.position = new Vector2(-gridW / 2 + tileSize / 2, gridH / 2 - tileSize / 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public GridManager gridScript;
+
+     
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       // gameObject.transform.position = gridScript.tile[1,1].transform.position;
+        Debug.Log(gridScript.tile[1,1].transform.position);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
