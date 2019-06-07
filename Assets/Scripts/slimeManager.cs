09c0d2e26986af7b31f41d5af3d9ff9ee3022385
hotelbones <hotelbones@gmail.com>
2019-06-07 18:45:57 -0400

    Slime spawns correctly

--- Assets/Scripts/slimeManager.cs
+++ Assets/Scripts/slimeManager.cs
@@ -14,10 +14,7 @@ public class slimeManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        yMin = gridScript.myxoMinPosY;
-        yMax = gridScript.myxoMaxPosY;
-        xMin = gridScript.myxoMinPosX;
-        xMax = gridScript.myxoMaxPosX;
+        
         
     }
 
@@ -28,22 +25,37 @@ public class slimeManager : MonoBehaviour
     }
 
     public void GenerateSlime(){
+        yMin = gridScript.myxoMinPosY;
+        yMax = gridScript.myxoMaxPosY;
+        xMin = gridScript.myxoMinPosX;
+        xMax = gridScript.myxoMaxPosX;
         slimeTile = new GameObject[40, 40];
         GameObject referenceSlime = (GameObject)Instantiate(Resources.Load("myxoTile"));
-         for (int row = 0; row < gridScript.rows; row++)
-        {
-            for (int col = 0; col < gridScript.cols; col++)
-            {
-                if (col >= xMin && row >= yMin && col <= xMax && row <= yMax){
-                    slimeTile[col,row] = (GameObject)Instantiate(referenceSlime, transform, false);
-                    slimeTile[col,row].transform.SetParent(referenceSlime.transform, false);
-                    slimeTile[col,row].name = "slimeTile [" + col + "," + row + "]";
+        //  for (int row = 0; row < gridScript.rows; row++)
+        // {
+        //     for (int col = 0; col < gridScript.cols; col++)
+        //     {
+        //         if (col >= xMin && row >= yMin && col <= xMax && row <= yMax){
+        //             slimeTile[col,row] = (GameObject)Instantiate(referenceSlime, transform, false);
+        //             slimeTile[col,row].transform.SetParent(referenceSlime.transform, false);
+        //             slimeTile[col,row].name = "slimeTile [" + col + "," + row + "]";
 
-                    float posX = col * gridScript.tileSize;
-                    float posY = row * -gridScript.tileSize;
-                    slimeTile[col,row].transform.position = new Vector2(posX, posY);
-                }
+        //             float posX = col * gridScript.tileSize;
+        //             float posY = row * -gridScript.tileSize;
+        //             slimeTile[col,row].transform.position = new Vector2(posX, posY);
+        //         }
+            // }
+        // }
+        for (int col = xMin; col <= xMax; col++){
+            //Debug.Log(xMin);
+            for (int row = yMin; row <= yMax; row++){
+                slimeTile[col,row] = (GameObject)Instantiate(referenceSlime, new Vector2(gridScript.tile[col,row].transform.position.x,gridScript.tile[col,row].transform.position.y), Quaternion.identity);
+                 slimeTile[col,row].transform.SetParent(gridScript.gameObject.transform, true);
+                slimeTile[col,row].name = "slimeTile [" + col + "," + row + "]";
+                Debug.Log(gridScript.tile[col,row]);
+                slimeTile[col,row].transform.position = gridScript.tile[col,row].transform.position;
             }
         }
+        Destroy(referenceSlime);
     }
 }
